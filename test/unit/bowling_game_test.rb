ab533ea1d115a3386b5108c9f7c5a8fa3a8214e3
antoniotirendi <antonio.tirendi@trovaprezzi.it>
2016-10-28 15:33:34 +0200

    more readable tests

--- test/unit/bowling_game_test.rb
+++ test/unit/bowling_game_test.rb
@@ -6,12 +6,12 @@ class BowlingGameTest < Test::Unit::TestCase
   end
 
   def test_gutter_game
-    20.times { @game.roll(0) }
+    10.times { 2.times { @game.roll(0) } }
     assert_equal 0, @game.score
   end
 
   def test_all_ones
-    20.times { @game.roll(1) }
+    10.times { 2.times { @game.roll(1) } }
     assert_equal 20, @game.score
   end
 
@@ -19,12 +19,13 @@ class BowlingGameTest < Test::Unit::TestCase
     @game.roll(3)
     @game.roll(7)
     @game.roll(4)
-    11.times { @game.roll(0) }
+    @game.roll(0)
+    8.times { 2.times { @game.roll(0) } }
     assert_equal 18, @game.score
   end
 
   def test_final_spare
-    18.times { @game.roll(0) }
+    9.times { 2.times { @game.roll(0) } }
     @game.roll(3)
     @game.roll(7)
     @game.roll(2)
@@ -35,12 +36,12 @@ class BowlingGameTest < Test::Unit::TestCase
     @game.roll(10)
     @game.roll(2)
     @game.roll(4)
-    16.times { @game.roll(0) }
+    8.times { 2.times { @game.roll(0) } }
     assert_equal 22, @game.score
   end
 
   def test_final_strike
-    18.times { @game.roll(0) }
+    9.times { 2.times { @game.roll(0) } }
     @game.roll(10)
     @game.roll(7)
     @game.roll(2)
